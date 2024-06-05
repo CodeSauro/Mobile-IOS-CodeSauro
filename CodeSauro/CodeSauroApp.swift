f1686e74335af3ede22485ccd83e451d073b2019
Argusto Lima <augusto.vlima2@gmai.com>
2024-06-05 10:09:26 -0300

    Initial Commit

---
+++ CodeSauro/CodeSauroApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeSauroApp.swift
+//  CodeSauro
+//
+//  Created by Augusto Lima on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeSauroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
